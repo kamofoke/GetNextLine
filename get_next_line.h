e9479a46868dbe4e8ac7a215d01427bdd0ca1a4b
Katleho MOFOKENG <kamofoke@c5r6s9.wethinkcode.co.za>
2019-06-28 14:13:25 +0200

    update

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,15 @@
 /*   By: kamofoke <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 10:32:28 by kamofoke          #+#    #+#             */
-/*   Updated: 2019/06/28 10:32:30 by kamofoke         ###   ########.fr       */
+/*   Updated: 2019/06/28 14:11:09 by kamofoke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <unistd.h>
 
+int		get_next_line(const int fd, char **line);
+
+#endif
